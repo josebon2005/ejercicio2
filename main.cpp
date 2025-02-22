91a0b71286c052c4f6d84a977e925af73b488b28
josebon2005 <jbonillas1@miumg.edu.gt>
2025-02-21 22:53:36 -0600

    Ingrese numeros positivos y sumelos al poner negativos

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Prototipos de funciones
 void imprimirNumeros();
 void imprimirHastaN();
+void sumarPositivos();
 
 int main() {
     int opcion;
@@ -13,22 +14,17 @@ int main() {
         cout << "\n--- MENU DE EJERCICIOS ---\n";
         cout << "1. Imprimir los numeros del 1 al 20 en orden ascendente\n";
         cout << "2. Imprimir los numeros desde 1 hasta un numero ingresado\n";
+        cout << "3. Sumar numeros positivos hasta ingresar un numero negativo\n";
         cout << "0. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
 
         switch (opcion) {
-            case 1:
-                imprimirNumeros();
-            break;
-            case 2:
-                imprimirHastaN();
-            break;
-            case 0:
-                cout << "Saliendo del programa...\n";
-            break;
-            default:
-                cout << "Opcion invalida, intente de nuevo.\n";
+            case 1: imprimirNumeros(); break;
+            case 2: imprimirHastaN(); break;
+            case 3: sumarPositivos(); break;
+            case 0: cout << "Saliendo del programa...\n"; break;
+            default: cout << "Opcion invalida, intente de nuevo.\n";
         }
 
     } while (opcion != 0);
@@ -57,4 +53,18 @@ void imprimirHastaN() {
         i++;
     }
     cout << endl;
+}
+
+// Función que suma números positivos hasta que el usuario ingrese un número negativo
+void sumarPositivos() {
+    int num, suma = 0;
+    cout << "\nIngrese numeros positivos (ingrese un numero negativo para terminar):\n";
+
+    while (true) {
+        cin >> num;
+        if (num < 0) break;  // Termina cuando se ingresa un número negativo
+        suma += num;
+    }
+
+    cout << "La suma de los numeros ingresados es: " << suma << endl;
 }
